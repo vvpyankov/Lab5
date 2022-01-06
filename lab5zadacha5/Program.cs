e1ff61e5eda75c62ca47376d85424971b14d0802
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-06 14:27:56 +0300

    06/01/2022

---
+++ lab5zadacha5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5zadacha5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число, определяющее размер двумерного массива:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = 1 - (i + j) % 2;
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
